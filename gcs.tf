5332fa8783ec9a09afcebebe91fd081bfadeb1bb
sainaveen143 <109063142+sainaveen143@users.noreply.github.com>
2023-09-15 11:23:23 +0530

    Update gcs.tf

--- gcs.tf
+++ gcs.tf
@@ -1,1 +1,7 @@
-
+resource "google_storage_bucket" "auto-expire" {
+  name          = "no-public-access-bucket"
+  location      = "US"
+  force_destroy = true
+  project       = "poc-ciscld-apigx-766888"
+  public_access_prevention = "enforced"
+}
